cca0e4ecd0b4764cb5ed06894398e42780b56887
RECODOM\marco.blay <marco.alain@recotech.es>
2021-04-12 11:37:08 +0200

    * Simbols definitions

---
+++ Editor/AddLogxDefineSymbols.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace ExceptionSoftware.Logs
+{
+
+    [InitializeOnLoad]
+    public class AddLogxDefineSymbols : Editor
+    {
+
+        /// <summary>
+        /// Symbols that will be added to the editor
+        /// </summary>
+        public static readonly string[] Symbols = new string[] {
+         "EXLOGS"
+     };
+
+        /// <summary>
+        /// Add define symbols as soon as Unity gets done compiling.
+        /// </summary>
+        static AddLogxDefineSymbols()
+        {
+            string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            List<string> allDefines = definesString.Split(';').ToList();
+            allDefines.AddRange(Symbols.Except(allDefines));
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(
+                EditorUserBuildSettings.selectedBuildTargetGroup,
+                string.Join(";", allDefines.ToArray()));
+        }
+
+    }
+}
